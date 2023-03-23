df1be33c13b19cd99820d145d394d6ae1bff8e39
kk66666666 <115142452+kk66666666@users.noreply.github.com>
2023-03-23 16:34:20 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+radio.onReceivedValue(function (name, value) {
+    if (name == "9") {
+        basic.showString("" + (value))
+        basic.pause(100)
+        radio.sendValue("10", value)
+    }
+})
+radio.setGroup(1)
+radio.setTransmitPower(7)
+basic.forever(function () {
+	
+})
